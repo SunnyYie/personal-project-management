fb83774a08ad40f400a13e62573fd165ba8a94e3
sun yi <86505029+SunnyYie@users.noreply.github.com>
2025-01-12 17:01:44 +0800

    feat：初始化nextjs端管理系统

---
+++ nextjs/client/app/components/header/components/breadcrumb.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export function LayoutBreadcrumb() {
+  return (
+    <Breadcrumb className="bg-gray-100 px-4 py-2">
+      <BreadcrumbList>
+        {/* {pathname.split("/").map((path, index) => {
+          if (path === "") {
+            return null;
+          }
+
+          if (index === pathname.split("/").length - 1) {
+            return <BreadcrumbPage key={index}>{path}</BreadcrumbPage>;
+          }
+
+          return (
+            <BreadcrumbItem key={index}>
+              <BreadcrumbLink href={`/${path}`}>{path}</BreadcrumbLink>
+              <BreadcrumbSeparator />
+            </BreadcrumbItem>
+          );
+        })} */}
+
+        <BreadcrumbItem className="hidden md:block">
+          <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator className="hidden md:block" />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
---
+++ nextjs/client/app/components/header/index.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Search, User, Settings } from "lucide-react";
+import { ModeToggle } from "./components/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-10 w-full border-b bg-background">
+      <div className="container flex h-16 items-center justify-between px-4">
+        <div className="flex items-center space-x-4">
+          <Button variant="ghost" size="icon" asChild>
+            <SidebarTrigger className="-ml-1" />
+          </Button>
+          <div className="relative hidden sm:block">
+            <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+            <Input
+              type="search"
+              placeholder="Search..."
+              className="w-64 pl-8"
+            />
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <ModeToggle />
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <User className="h-5 w-5" />
+                <span className="sr-only">User Menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Log out</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <Button variant="ghost" size="icon">
+            <Settings className="h-5 w-5" />
+            <span className="sr-only">Settings</span>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
